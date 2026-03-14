d015410d60f380e71833b958d67c6234b32348d4
reversearrow <reversearrow@gmail.com>
2026-03-14 04:36:53 -0400

    feat: 204

--- 0204/table.go
+++ 0204/table.go
@@ -7,14 +7,40 @@ type DB struct {
 func (db *DB) Open() error  { return db.KV.Open() }
 func (db *DB) Close() error { return db.KV.Close() }
 
-func (db *DB) Select(schema *Schema, row Row) (ok bool, err error)
+func (db *DB) Select(schema *Schema, row Row) (ok bool, err error) {
+	k := row.EncodeKey(schema)
+	val, ok, err := db.KV.Get(k)
+	if !ok || err != nil {
+		return ok, err
+	}
+	if err := row.DecodeVal(schema, val); err != nil {
+		return false, err
+	}
 
-func (db *DB) Insert(schema *Schema, row Row) (updated bool, err error)
+	return true, nil
+}
 
-func (db *DB) Upsert(schema *Schema, row Row) (updated bool, err error)
+func (db *DB) Insert(schema *Schema, row Row) (updated bool, err error) {
+	k := row.EncodeKey(schema)
+	v := row.EncodeVal(schema)
+	return db.KV.SetEx(k, v, ModeInsert)
+}
 
-func (db *DB) Update(schema *Schema, row Row) (updated bool, err error)
+func (db *DB) Upsert(schema *Schema, row Row) (updated bool, err error) {
+	k := row.EncodeKey(schema)
+	v := row.EncodeVal(schema)
+	return db.KV.SetEx(k, v, ModeUpsert)
+}
 
-func (db *DB) Delete(schema *Schema, row Row) (deleted bool, err error)
+func (db *DB) Update(schema *Schema, row Row) (updated bool, err error) {
+	k := row.EncodeKey(schema)
+	v := row.EncodeVal(schema)
+	return db.KV.SetEx(k, v, ModeUpdate)
+}
+
+func (db *DB) Delete(schema *Schema, row Row) (deleted bool, err error) {
+	k := row.EncodeKey(schema)
+	return db.KV.Del(k)
+}
 
 // QzBQWVJJOUhU https://trialofcode.org/
